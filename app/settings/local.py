89a0cdff8262368e9dadcaf8e4bcd97cfd46005a
Philip Forget <philipforget@gmail.com>
2012-12-19 17:33:48 -0500

    site name, no more sites

--- app/settings/local.py
+++ app/settings/local.py
@@ -1,13 +1,14 @@
 from .base import *
 
 
+SITE_NAME = 'dev.byebyebirdie:8000'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(REPO_ROOT, 'tmp', 'bbb.sqlite'),
     }
 }
-
 
 CACHES = {
     'default': {
