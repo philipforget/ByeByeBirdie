579c85823000946f8474b5d4a143bdb13bfd6e80
Philip Forget <philipforget@gmail.com>
2012-12-19 16:07:42 -0500

    urls

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 
+
+
 urlpatterns = patterns('main.views',
-    url(r'^/logout/$', 'logout', name='logout'),
-    url(r'^/?$', 'home', name='home'),
-    url(r'^(?P<username>\w+)/?$', 'user_page', name='user_page'),
+    url(r'^$', 'home', name='home'),
+    url(r'^logout$', 'logout', name='logout'),
+    url(r'^(?P<username>\w+)$', 'user_page', name='user_page'),
 )
--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
 admin.autodiscover()
 
 
 
 urlpatterns = patterns('',
-    url(r'^$', include('main.urls', namespace='main')),
-    url(r'^auth/', include('social_auth.urls', namespace='social')),
+    url(r'', include('main.urls')),
+    url(r'^auth/', include('social_auth.urls')),
 )
