2c6e63fed021e3b54d15c5eff25c3ce426fb1cd4
Philip Forget <philipforget@gmail.com>
2012-12-20 13:04:36 -0500

    validate message

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,5 @@
 from annoying.decorators import ajax_request
+from django import forms
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotAllowed, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
@@ -37,13 +38,19 @@ def unfollow(request):
     if request.POST.get('message', None) is None:
         return HttpResponseBadRequest(
             'no "message" post paramater found')
-    
+
+    m = forms.TextField(max_length=500)
+
+    try:
+        message = m.clean(request.POST['message'])
+    except forms.ValidationError as e:
+        return HttpResponseBadRequest(
+            e.messages[0])
+
     try:
         unfollow = request.user.unfollow(
-            request.POST['username'], request.POST['message'])
-
+            request.POST['username'], message)
     except Exception as e:
-        print e
         return HttpResponseBadRequest(
             'Error creating unfollow. You must be following the user')
 
@@ -51,3 +58,12 @@ def unfollow(request):
         'success': True,
         'unfollow': unfollow.serialize()
     }
+
+
+@login_required
+@ajax_request
+@csrf_exempt
+def opt_out(request):
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(
+            'Accepted methods: POST')
