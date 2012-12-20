c1908faffad40bde75176c1d48324378986fe58e
robertjpetro@gmail.com <robertjpetro@gmail.com>
2012-12-20 11:02:37 -0500

    homepage stuff

--- media/js/views/homeView.js
+++ media/js/views/homeView.js
@@ -2,18 +2,143 @@ App.HomeView = Backbone.View.extend({
 	el: '#main',
 
 	events: {
-		"focus #unfollow input": "autocomplete"
+		"focus #unfollow input": "autocomplete",
+		"submit #unfollow form": "goToUser"
 	},
 	
 	initialize: function() {
-        //this.template = _.template(TPL.get('home'));
 		this.render();
 	},
 
 	render: function() {
+		this.$el.hide().fadeIn(600);
+
+		if ($('#login').is(':visible')) {
+			this.setupHome();
+		}
 
 		return this;
 	},
+
+	setupHome: function() {
+		var arcPeople = [
+			'robertjpetro',
+			'philipforget',
+			'cshoe_',
+			'redkeg',
+			'mciarlo',
+			'd',
+			'd_run',
+			'alecmunro',
+			'jpotisch',
+			'gooeyblob',
+			'desireedigenio',
+			'JustinIso',
+			'KareemYoussef',
+			'avi4now',
+			'xsheenasinghx',
+			'timothymeaney',
+			'closacco',
+			'simadown',
+			'bowmanb',
+			'thegaw',
+			'dadalavida',
+			'readability',
+			'jimniels',
+			'zeeshanlakhani',
+			'xxbobbyzxx',
+			'billsmartt',
+			'robertjpetro',
+			'philipforget',
+			'cshoe_',
+			'redkeg',
+			'mciarlo',
+			'd',
+			'd_run',
+			'alecmunro',
+			'jpotisch',
+			'gooeyblob',
+			'desireedigenio',
+			'JustinIso',
+			'KareemYoussef',
+			'avi4now',
+			'xsheenasinghx',
+			'timothymeaney',
+			'closacco',
+			'simadown',
+			'bowmanb',
+			'thegaw',
+			'dadalavida',
+			'readability',
+			'jimniels',
+			'zeeshanlakhani',
+			'xxbobbyzxx',
+			'billsmartt',
+			'robertjpetro',
+			'philipforget',
+			'cshoe_',
+			'redkeg',
+			'mciarlo',
+			'd',
+			'd_run',
+			'alecmunro',
+			'jpotisch',
+			'gooeyblob',
+			'desireedigenio',
+			'JustinIso',
+			'KareemYoussef',
+			'avi4now',
+			'xsheenasinghx',
+			'timothymeaney',
+			'closacco',
+			'simadown',
+			'bowmanb',
+			'thegaw',
+			'dadalavida',
+			'readability',
+			'jimniels',
+			'zeeshanlakhani',
+			'xxbobbyzxx',
+			'billsmartt',
+			'JustinIso',
+			'KareemYoussef',
+			'avi4now',
+			'xsheenasinghx',
+			'timothymeaney',
+			'closacco',
+			'simadown',
+			'bowmanb',
+			'thegaw',
+			'dadalavida',
+			'readability',
+			'jimniels',
+			'zeeshanlakhani',
+			'xxbobbyzxx',
+			'bowmanb',
+			'thegaw',
+			'dadalavida',
+			'readability',
+			'jimniels',
+			'zeeshanlakhani',
+			'xxbobbyzxx'
+		],
+		randPeople = arcPeople.sort(function() { return 0.5 - Math.random() }),
+		homeTile = $('#home-tile');
+
+		$.each(randPeople, function(i, name) {
+			var li = $('<li/>').appendTo(homeTile),
+				arcImg = $('<img />')
+					.attr('src', 'http://api.twitter.com/1/users/profile_image/' + name)
+					.appendTo(li);
+		});
+
+		randLi = $('#home-tile li img').sort(function() { return 0.5 - Math.random() });
+
+		$.each(randLi, function(i) {
+			$(this).delay(1000*i).fadeOut();
+		})
+	},
+
 	// /api/v1/me/following
 	autocomplete: function(e) {
 		$.get('/api/v1/me/following', function(data) {
@@ -32,18 +157,28 @@ App.HomeView = Backbone.View.extend({
                 	})
 	            },
 	            focus: function( event, ui ) {
-	            	var name = ui.item.label.split(':')[1];
+	            	var name = ui.item.label.split(':')[1],
+	            		screen_name = ui.item.label.split(':')[0];
 
-   					$('#unfollow-input').val( name );	
+   					$('#unfollow-input').val(name);
+   					$('#unfollow-hidden').val(screen_name);
                 	return false;
             	},
             	select: function( event, ui ) {
-	            	var name = ui.item.label.split(':')[1];
+	            	var name = ui.item.label.split(':')[1],
+	            		screen_name = ui.item.label.split(':')[0];
 
-   					$('#unfollow-input').val( name );	
+   					$('#unfollow-input').val(name);	
+   					$('#unfollow-hidden').val(screen_name);
                 	return false;
             	}
 	        });
 		});
+    },
+
+    goToUser: function() {
+    	var userName = $('#unfollow-hidden').val();
+
+    	window.location.href = '/' + userName ;
     }
 });
