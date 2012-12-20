ada800c70a3e6665949dadd2dd39294d2cc7f13b
Philip Forget <philipforget@gmail.com>
2012-12-20 14:10:06 -0500

    boom

--- app/main/urls.py
+++ app/main/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic import TemplateView
 
 from . import views
 
-
-dtt = 'django.views.generic.simple.direct_to_template'
 
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^logout$', views.logout, name='logout'),
-    url(r'^404$', dtt, {'template': '404.html'}),
-    url(r'^500$', dtt, {'template': '500.html'}),
+    url(r'^404$', TemplateView.as_view(template_name='404.html')),
+    url(r'^500$', TemplateView.as_view(template_name='500.html')),
     url(r'^(?P<username>\w+)$', views.user_page, name='user_page'),
 )
